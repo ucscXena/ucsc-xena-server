8b04c950d88865befa1337df370af12dd3f29ece
Brian Craft <craft@soe.ucsc.edu>
2013-08-22 13:49:43 -0700

    Update project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cavm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Cancer Analytics Virtual Machine"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
