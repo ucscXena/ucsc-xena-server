981faa5a5e07c732a71db96a81219058e0a9aa55
Brian Craft <craft@soe.ucsc.edu>
2014-05-15 13:41:48 -0700

    whitespace

--- src/cavm/db.clj
+++ src/cavm/db.clj
@@ -4,6 +4,7 @@
   "Xena database protocol"
   (write-matrix [this mname files metadata data-fn features always]
                 "Write a matrix to storage")
-  (write-probemap [this pname files metadata data-fn always] "Write a probemap to storage")
+  (write-probemap [this pname files metadata data-fn always]
+                  "Write a probemap to storage")
   (run-query [this query] "Execute a metadata sql query")
   (close [this] "Close the database"))
