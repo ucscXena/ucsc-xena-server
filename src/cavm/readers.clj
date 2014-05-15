ee262e5c7256d03219c15114abf4e4f1ccfb5d73
Brian Craft <craft@soe.ucsc.edu>
2014-05-14 18:40:18 -0700

    Update readers docs.

--- src/cavm/readers.clj
+++ src/cavm/readers.clj
@@ -5,16 +5,19 @@
 ;
 ; Readers are split into two parts: a file type detector, and a reader multimethod.
 ; This split is maintained because there is a not a one-to-one relationship between
-; dectecting a file type and having a reader, e.g. we identify all cgdata file types
-; with a single method that reads file type from the json metadata.
-;
-; XXX Note that this strategy opens each file repeatedly to determine its type.
+; detecting a file type and having a reader, e.g. we identify all cgdata file types
+; with a single method that the reads file type from the json metadata.
 ;
 
 (defmulti reader
-  (fn [filetype url] filetype))
+  (fn [filetype file] filetype))
 
-(defn detector [& detectors]
+; XXX Note that this strategy opens each file repeatedly to determine its type.
+(defn detector
+  "Returns a function that invokes file type detectors in order
+  until the file is identified, then returns the file type and its
+  registered reader"
+  [& detectors]
   (fn [file]
     (let [file-type (some #(% file) detectors)]
       {:file-type file-type
