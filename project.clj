6bacb43317ecf69513ed0c3c751b8b8247c7afaa
Brian Craft <craft@soe.ucsc.edu>
2014-05-21 10:13:12 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cavm "0.1.0-SNAPSHOT"
+(defproject cavm "0.1.1-SNAPSHOT"
   :description "Cancer Analytics Virtual Machine"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
