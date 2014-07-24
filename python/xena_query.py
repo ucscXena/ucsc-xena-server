97fd65e4fc7e73ecaf9ecb05dc71506d668778ca
Brian Craft <craft@soe.ucsc.edu>
2014-07-24 14:53:16 -0700

    Generalize sample lookup by field.

--- python/xena_query.py
+++ python/xena_query.py
@@ -13,18 +13,29 @@ Queries are scheme expressions.
 
 Looking up sample ids for the TCGA LGG cohort.
 
->>> xena.post("https://genome-cancer.ucsc.edu/proj/public/xena",
-               xena.patient_to_sample_query("TCGA.LGG.sampleMap",
-                                            ["TCGA-CS-4938",
-                                             "TCGA-HT-7693",
-                                             "TCGA-CS-6665",
-                                             "TCGA-S9-A7J2",
-                                             "TCGA-FG-A6J3"]))
+>>> r = xena.post("https://genome-cancer.ucsc.edu/proj/public/xena",
+                  xena.patient_to_sample_query("TCGA.LGG.sampleMap",
+                                               ["TCGA-CS-4938",
+                                                "TCGA-HT-7693",
+                                                "TCGA-CS-6665",
+                                                "TCGA-S9-A7J2",
+                                                "TCGA-FG-A6J3"]))
+'{"TCGA.LGG.sampleMap":["TCGA-CS-4938-01","TCGA-CS-6665-01","TCGA-FG-A6J3-01","TCGA-HT-7693-01","TCGA-S9-A7J2-01"]}'
+
+>>> r = xena.post("https://genome-cancer.ucsc.edu/proj/public/xena",
+                  xena.find_sample_by_field_query("TCGA.LGG.sampleMap",
+                                                    "_PATIENT",
+                                                    ["TCGA-CS-4938",
+                                                     "TCGA-HT-7693",
+                                                     "TCGA-CS-6665",
+                                                     "TCGA-S9-A7J2",
+                                                     "TCGA-FG-A6J3"]))
 '{"TCGA.LGG.sampleMap":["TCGA-CS-4938-01","TCGA-CS-6665-01","TCGA-FG-A6J3-01","TCGA-HT-7693-01","TCGA-S9-A7J2-01"]}'
 >>> import json
->>> json.loads(_)
+>>> json.loads(r)
 {u'TCGA.LGG.sampleMap': [u'TCGA-CS-4938-01', u'TCGA-CS-6665-01', u'TCGA-FG-A6J3-01', u'TCGA-HT-7693-01', u'TCGA-S9-A7J2-01']}
 """
+
 import urllib2
 
 def quote(s):
@@ -35,31 +46,35 @@ def array_fmt(l):
 
 sample_query_str = """
 (let [cohort %s
-      patient-dataset (car (query {:select [[:field.id :patient] [:dataset.id :dataset]]
-                                   :from [:dataset]
-                                   :left-join [:field [:= :dataset_id :dataset.id]]
-                                   :where [:and [:= :cohort cohort]
-                                                [:= :field.name "_PATIENT"]]}))
-      patient (:PATIENT patient-dataset)
-      dataset (:DATASET patient-dataset)
+      field_id-dataset (car (query {:select [[:field.id :field_id] [:dataset.id :dataset]]
+                                    :from [:dataset]
+                                    :left-join [:field [:= :dataset_id :dataset.id]]
+                                    :where [:and [:= :cohort cohort]
+                                                 [:= :field.name %s]]}))
+      field_id (:FIELD_ID field_id-dataset)
+      dataset (:DATASET field_id-dataset)
       sample (:ID (car (query {:select [:field.id]
                                :from [:field]
                                :where [:and [:= :dataset_id dataset]
                                             [:= :field.name "sampleID"]]})))
       N (- (/ (:N (car (query {:select [[#sql/call [:sum #sql/call [:length :scores]] :N]]
-                  :from [:field_score]
-                  :join [:scores [:= :scores_id :scores.id]]
-                  :where [:= :field_id patient]}))) 4) 1)]
+                               :from [:field_score]
+                               :join [:scores [:= :scores_id :scores.id]]
+                               :where [:= :field_id field_id]}))) 4) 1)]
   {cohort (map :SAMPLE (query {:select [:sample]
-                               :from [{:select [[#sql/call [:unpackValue patient, :x] :patient]
+                               :from [{:select [[#sql/call [:unpackValue field_id, :x] :field_value]
                                                 [#sql/call [:unpackValue sample, :x]  :sample]]
                                        :from [#sql/call [:system_range 0 N]]}]
-                               :where [:in :patient %s]}))})
+                               :where [:in :field_value %s]}))})
 """
+
+def find_sample_by_field_query(cohort, field, values):
+    """Return a xena query which looks up sample ids for the given field=values."""
+    return sample_query_str % (quote(cohort), quote(field), array_fmt(values))
 
 def patient_to_sample_query(cohort, patients):
     """Return a xena query which looks up sample ids for the given patients."""
-    return sample_query_str % (quote(cohort), array_fmt(patients))
+    return find_sample_by_field_query(cohort, "_PATIENT", patients)
 
 headers = { 'Content-Type' : "text/plain" }
 
