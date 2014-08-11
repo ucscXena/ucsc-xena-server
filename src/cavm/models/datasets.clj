45807a4bb28007191e187af573596c8cc3043c04
Brian Craft <craft@soe.ucsc.edu>
2014-08-11 13:19:40 -0700

    Drop cavm.models.datasets.

--- src/cavm/models/datasets.clj
+++
@@ -1,7 +0,0 @@
-(ns cavm.models.datasets
-  (:require [cavm.h2 :as db])
-  (:gen-class))
-
-(defn query [db q]
-  (db/with-db db
-    (db/run-query q)))
--- src/cavm/views/datasets.clj
+++ src/cavm/views/datasets.clj
@@ -1,6 +1,5 @@
 (ns cavm.views.datasets
   (:use [clojure.core.matrix])
-  (:require [cavm.models.datasets :as d])
   (:require [cavm.db :as cdb])
   (:require [cavm.query.expression :as expr])
   (:require [cavm.query.functions :as f])
@@ -18,49 +17,6 @@
   (:require [taoensso.timbre.profiling :as profiling :refer [p profile]])
   (:gen-class))
 
-; disabling old noir pages for now.
-(defmacro defpage [& args])
-
-(defpage [:get "/datasets"] []
-         (response/json (d/datasets)))
-
-; XXX temporary alias, to interoperate with client
-(defpage [:get "/tracks"] []
-  (response/json (d/datasets)))
-
-; XXX for CORS?
-(defpage [:options [":any" :any #".*"]] []
-  (response/empty))
-
-(defn genes-decode [genes]
-  (map codec/url-decode (clojure.string/split genes #",")))
-
-(defpage [:get ["/datasets/:id" :id #".+"]] {:keys [id genes]}
-  (response/json (d/dataset-by-genes id (genes-decode genes))))
-
-(defpage [:get ["/query/:query" :query #".+"]] {:keys [query]}
-  (let [{db :db} (ring-request)]
-    (response/json (d/query db (edn/read-string query)))))
-
-; pretty-print query
-(defpage [:get ["/query-p/:query" :query #".+"]] {:keys [query]}
-  (let [{db :db} (ring-request)]
-    (response/content-type "application/json; charset=utf-8"
-                           (binding [clojure.pprint/*print-right-margin* 200]
-                             (with-out-str (json/encode (d/query db (edn/read-string query)) {:pretty true}))))))
-
-(defn samples-for-attr [attr]
-  {:select [[:experiments.name "experiment"] :exp_samples.name]
-   :from [:exp_samples]
-   :left-join [:experiments [:= :exp_samples.experiments_id :experiments.id]]
-   :where [:in :experiments.id
-           {:select [:experiments.id]
-            :modifiers [:distinct]
-            :from [:features]
-            :left-join [:probes [:= :features.probes_id :probes.id]
-                        :experiments [:= :probes.eid :experiments.id]]
-            :where [:= :features.longTitle attr]}]})
-
 ; XXX This is probably not the fastest way to do
 ; this.
 (defn- boolean-matrix [m pred]
@@ -70,24 +26,6 @@
   (->> (map vector samples matches)
        (filter second)
        (map first)))
-
-(defn attr-filter [attr pred]
-  (->> attr
-       (samples-for-attr)
-       (h2/run-query)
-       (group-by :EXPERIMENT)
-       (map (fn [[exp samps]]
-              {'table exp
-               'columns [attr]
-               'samples (map :NAME samps)}))
-       (map #(assoc % 'data (sources/read-symbols [%])))
-       (map #(assoc % 'samples
-                    (filter-matches ('samples %)
-                                    (boolean-matrix
-                                      (first ('data %)) pred))))
-       (mapcat #(map (fn [exp samp]
-                         {:experiment exp
-                          :sample samp}) (repeat ('table %)) ('samples %)))))
 
 (defn filter-attr [samples attr pred]
   (->> samples
