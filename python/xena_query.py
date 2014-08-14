b3537a4ff43b314691d39d55ad4d048996fe92d0
Brian Craft <craft@soe.ucsc.edu>
2014-08-14 11:31:13 -0700

    Update query for performance.

--- python/xena_query.py
+++ python/xena_query.py
@@ -38,12 +38,31 @@ Looking up sample ids for the TCGA LGG cohort.
 
 import urllib2
 
+def compose1(f, g):
+    def composed(*args, **kwargs):
+        return f(g(*args, **kwargs))
+    return composed
+
+# funcitonal composition, e.g.
+# compose(f, g)(a, ...) == f(g(a, ...))
+compose = lambda *funcs: reduce(compose1, funcs)
+
 def quote(s):
     return '"' + s + '"'
 
 def array_fmt(l):
     return '[' + ', '.join((quote(s) for s in l)) + ']'
 
+# The strategy here is
+#   o Do table scan on code to find codes matching field values
+#   o Do IN query on unpack(field, x) to find rows matching codes
+#   o Project to unpack(sample, x) to get sampleID code
+#   o Join with code to get sampleID values
+#
+# Note the :limit on the table scan. This makes the table scan exit after we've
+# found enough values, rather than continuing to the end. We can do this because
+# enumerated values are unique. An alternative would be to index all the enumerated
+# values in the db.
 sample_query_str = """
 (let [cohort %s
       field_id-dataset (car (query {:select [[:field.id :field_id] [:dataset.id :dataset]]
@@ -51,6 +70,7 @@ sample_query_str = """
                                     :join [:field [:= :dataset_id :dataset.id]]
                                     :where [:and [:= :cohort cohort]
                                                  [:= :field.name %s]]}))
+      values %s
       field_id (:field_id field_id-dataset)
       dataset (:dataset field_id-dataset)
       sample (:id (car (query {:select [:field.id]
@@ -60,11 +80,28 @@ sample_query_str = """
       N (- (:rows (car (query {:select [:rows]
                                :from [:dataset]
                                :where [:= :id dataset]}))) 1)]
-  {cohort (map :sample (query {:select [:sample]
-                               :from [{:select [[#sql/call [:unpackValue field_id, :x] :field_value]
-                                                [#sql/call [:unpackValue sample, :x]  :sample]]
-                                       :from [#sql/call [:system_range 0 N]]}]
-                               :where [:in :field_value %s]}))})
+  {cohort (map :value (query {:select [:value]
+                              :from [{:select [:x #sql/call [:unpack field_id, :x]]
+                                      :from [#sql/call [:system_range 0 N]]
+                                      :where [:in #sql/call [:unpack field_id, :x] {:select [:ordering]
+                                                                                             :from [:code]
+                                                                                             :where [:and [:= :field_id field_id]
+                                                                                                          [:in :value values]]
+                                                                                             :limit (count values)}]}]
+                              :join [:code [:and [:= :field_id sample]
+                                                 [:= :ordering #sql/call [:unpack sample :x]]]]}))})
+"""
+
+cohort_query_str = """
+(map :cohort (query {:select [:%distinct.cohort]
+                     :from [:dataset]
+                     :where [:not [:is nil :cohort]]}))
+"""
+
+datasets_list_in_cohort_query = """
+(map :text (query {:select [:text]
+                   :from [:dataset]
+                   :where [:= :cohort %s ]})
 """
 
 def find_sample_by_field_query(cohort, field, values):
@@ -83,3 +120,14 @@ def post(url, query):
     response = urllib2.urlopen(req)
     result = response.read()
     return result
+
+def find_cohorts():
+    """ Return a list of cohorts on a host at a specific url """
+    """ return example: ["chinSF2007_public","TCGA.BRCA.sampleMap","cohort3"] """
+    return cohort_query_str
+
+def find_datasets_in_cohort(url, cohort):
+    """ Returen a list of datasets in a specific cohort on server=url """
+    """ Each dataset is a dictionary of the data's metadata"""
+    return map(json.loads,
+            json.loads(post(url, datasets_list_in_cohort_query % (quote(cohort)))))
