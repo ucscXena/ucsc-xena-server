e96c33948858a71cf46cee36c7375e2ca6b503d6
Jing <jingchunzhu@gmail.com>
2016-02-22 23:46:34 -0800

    updated xena_query.py API and added an examples.py file for users

---
+++ python/examples.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import xena_query as xena
+
+huburl = "https://genome-cancer.ucsc.edu/proj/public/xena"
+dataset = "public/TCGA/TCGA.PANCAN.sampleMap/HiSeqV2"
+samples = ["TCGA-44-6778-01","TCGA-44-6778-01"]
+identifiers = ["TP53"]
+
+
+# Find all the identifiers of a dataset
+allidentifiers = xena.dataset_field(huburl, dataset)
+print allidentifiers[-10:]
+#'[u'ZXDB', u'ZXDC', u'ZYG11A', u'ZYG11B', u'ZYX', u'ZZEF1', u'ZZZ3', u'psiTPTE22', u'sampleID', u'tAKR']'
+
+#Find all the samples of a dataset
+allsamples = xena.dataset_samples(huburl, dataset)
+print allsamples[:5]
+#'[u'TCGA-S9-A7J2-01', u'TCGA-G3-A3CH-11', u'TCGA-EK-A2RE-01', u'TCGA-44-6778-01', u'TCGA-VM-A8C8-01']'
+
+#Find value matrix of a particular set of samples and identifiers (genes, probes, isoforms)
+values = xena.dataset_probe_values(huburl, dataset, samples, identifiers)
+print values
+#[[10.4169, 10.4169]]
+
+
+
--- python/xena_query.py
+++ python/xena_query.py
@@ -11,6 +11,22 @@ Queries are scheme expressions.
 >>> xena.post("https://genome-cancer.ucsc.edu/proj/public/xena", "(let [x 2 y (+ x 3)] (* x y))")
 '10.0'
 
+Find all the identifiers of a dataset
+>>> probes = xena.dataset_field("https://genome-cancer.ucsc.edu/proj/public/xena", "public/TCGA/TCGA.PANCAN.sampleMap/HiSeqV2")
+>>> print probes[-10:]
+'[u'ZXDB', u'ZXDC', u'ZYG11A', u'ZYG11B', u'ZYX', u'ZZEF1', u'ZZZ3', u'psiTPTE22', u'sampleID', u'tAKR']'
+
+Find all the samples of a dataset
+>>> samples = xena.dataset_samples("https://genome-cancer.ucsc.edu/proj/public/xena", "public/TCGA/TCGA.PANCAN.sampleMap/HiSeqV2")
+>>> print samples[:5]
+'[u'TCGA-S9-A7J2-01', u'TCGA-G3-A3CH-11', u'TCGA-EK-A2RE-01', u'TCGA-44-6778-01', u'TCGA-VM-A8C8-01']'
+
+Find value matrix of a particular set of samples and identifiers (genes, probes, isoforms)
+>>> values = xena.dataset_probe_values("https://genome-cancer.ucsc.edu/proj/public/xena", "public/TCGA/TCGA.PANCAN.sampleMap/HiSeqV2",["TCGA-44-6778-01","TCGA-44-6778-01"],["TP53"])
+>>> print values
+[[10.4169, 10.4169]]
+
+
 Looking up sample ids for the TCGA LGG cohort.
 
 >>> r = xena.post("https://genome-cancer.ucsc.edu/proj/public/xena",
@@ -36,7 +52,7 @@ Looking up sample ids for the TCGA LGG cohort.
 {u'TCGA.LGG.sampleMap': [u'TCGA-CS-4938-01', u'TCGA-CS-6665-01', u'TCGA-FG-A6J3-01', u'TCGA-HT-7693-01', u'TCGA-S9-A7J2-01']}
 """
 
-import urllib2
+import urllib2, json
 import re
 
 def compose1(f, g):
@@ -64,34 +80,6 @@ def array_fmt(l):
 # found enough values, rather than continuing to the end. We can do this because
 # enumerated values are unique. An alternative would be to index all the enumerated
 # values in the db.
-sample_query_str = """
-(let [cohort %s
-      field_id-dataset (car (query {:select [[:field.id :field_id] [:dataset.id :dataset]]
-                                    :from [:dataset]
-                                    :join [:field [:= :dataset_id :dataset.id]]
-                                    :where [:and [:= :cohort cohort]
-                                                 [:= :field.name %s]]}))
-      values %s
-      field_id (:field_id field_id-dataset)
-      dataset (:dataset field_id-dataset)
-      sample (:id (car (query {:select [:field.id]
-                               :from [:field]
-                               :where [:and [:= :dataset_id dataset]
-                                            [:= :field.name "sampleID"]]})))
-      N (- (:rows (car (query {:select [:rows]
-                               :from [:dataset]
-                               :where [:= :id dataset]}))) 1)]
-  {cohort (map :value (query {:select [:value]
-                              :from [{:select [:x #sql/call [:unpack field_id, :x]]
-                                      :from [#sql/call [:system_range 0 N]]
-                                      :where [:in #sql/call [:unpack field_id, :x] {:select [:ordering]
-                                                                                             :from [:code]
-                                                                                             :where [:and [:= :field_id field_id]
-                                                                                                          [:in :value values]]
-                                                                                             :limit (count values)}]}]
-                              :join [:code [:and [:= :field_id sample]
-                                                 [:= :ordering #sql/call [:unpack sample :x]]]]}))})
-"""
 
 cohort_query_str = """
 (map :cohort (query {:select [:%distinct.cohort]
@@ -99,18 +87,41 @@ cohort_query_str = """
                      :where [:not [:is nil :cohort]]}))
 """
 
-datasets_list_in_cohort_query = """
-(map :text (query {:select [:text]
-                   :from [:dataset]
-                   :where [:= :cohort %s ]})
+datasets_list_in_cohort_str ="""
+(map :name (query {:select [:name :type :datasubtype :probemap :text :status]
+      :from [:dataset]
+      :where [:= :cohort %s]}))
 """
 
-datasets_type_pattern_str = """
-(map :name (query {:select [:name]
+dataset_type_str = """
+(map :type (query {:select [:type]
                    :from [:dataset]
-                   :where [:and [:= :type %s]
-                                [:like :name %s]]}))
+                   :where [:= :name %s]}))
 """
+
+dataset_field_str = """
+(map :name (query {:select [:field.name]
+             :from [:dataset]
+             :join [:field [:= :dataset.id :dataset_id]]
+             :where [:= :dataset.name %s]}))
+"""
+
+dataset_samples_str = """
+(map :value (query {:select [:value]
+            :from [:dataset]
+            :join [:field [:= :dataset.id :dataset_id]
+            :code [:= :field.id :field_id]]
+            :where [:and
+            [:= :dataset.name %s]
+            [:= :field.name "sampleID"]]}))
+"""
+
+dataset_probe_str = """
+    (fetch [{:table %s
+          :columns %s
+          :samples %s}])
+"""
+
 
 def find_sample_by_field_query(cohort, field, values):
     """Return a xena query which looks up sample ids for the given field=values."""
@@ -129,24 +140,32 @@ def post(url, query):
     result = response.read()
     return result
 
-def find_cohorts():
+def find_cohorts(url):
     """ Return a list of cohorts on a host at a specific url """
     """ return example: ["chinSF2007_public","TCGA.BRCA.sampleMap","cohort3"] """
-    return cohort_query_str
+    return json.loads(post(url,cohort_query_str))
 
-def find_datasets_in_cohort(url, cohort):
-    """ Return a list of datasets in a specific cohort on server=url.
-    Each dataset is a dictionary of the data's metadata.
-    This should be refactored to be consistent with the other methods."""
-    return map(json.loads,
-            json.loads(post(url, datasets_list_in_cohort_query % (quote(cohort)))))
+def dataset_field(host, dataset):
+    """return probes or features of a dataset"""
+    return json.loads(post(host, dataset_field_str % (quote(dataset))))
 
-def find_datasets_type_pattern(type, pattern):
-    """Return a xena query which returns a list of datasets
-    filtered by a pattern on the dataset name. The pattern is sql:
-    % is wildcard."""
-    return datasets_type_pattern_str % (quote(type), quote(pattern))
+def datasets_list_in_cohort (host, cohort):
+    """return datasets in a cohort"""
+    return json.loads(post(host, datasets_list_in_cohort_str % (quote(cohort))))
 
+def dataset_samples (host, dataset):
+    return json.loads(post(host, dataset_samples_str % (quote(dataset))))
+
+def dataset_probe_values (host, dataset, samples, probes):
+    """ return matrix of values [[x11, x12, ...],[x21, x22,...],...]"""
+    """ x11: sample 1 and probe 1"""
+    """ x12: sample 2 and probe 1"""
+    """ x21: sample 1 and probe 2"""
+    """ x22: sample 2 and probe 2"""
+    return json.loads(post(host, dataset_probe_str % (quote(dataset), array_fmt(probes), array_fmt(samples))))
+
+def dataset_type (host, dataset):
+    return json.loads(post(host, dataset_type_str % (quote(dataset))))
 
 def strip_first_url_dir(path):
     return re.sub(r'^[^/]*', '', path)
