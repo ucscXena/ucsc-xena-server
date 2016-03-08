4d0c6d5a499958630bef383f348d513295b92d8f
Brian Craft <bcboy@thecraftstudio.com>
2016-03-08 12:32:38 -0800

    Fix nesting of ct/testing.

--- test/cavm/sqleval_test.clj
+++ test/cavm/sqleval_test.clj
@@ -26,75 +26,75 @@
                                    [:e :f :h]))))))
 
 (ct/deftest eval-test
-  (ct/testing "eval")
-  (let [data {"a" [:a :b :c :d :e :f :g :h]
-              "b" (into [] (range 20 30))}
-        rows (apply sorted-set (range 8))
-        store {:indexed? #{}
-               :fetch (fn [rows-in field]
+  (ct/testing "eval"
+    (let [data {"a" [:a :b :c :d :e :f :g :h]
+                "b" (into [] (range 20 30))}
+          rows (apply sorted-set (range 8))
+          store {:indexed? #{}
+                 :fetch (fn [rows-in field]
                           (let [r (or rows-in rows)]
                             (zipmap r
                                     (map (data field) r))))}]
-    (ct/is (= {"b" [24 25 27]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["b"] :where [:in "a" [:e :f :h]]})))
-    (ct/is (= {"b" [24 25 27]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["b"] :where [:and [:in "a" [:e :f :h]]]})))
-    (ct/is (= {"a" [:e :f] "b" [24 25]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["a" "b"] :where [:and [:in "a" [:e :f :h]] [:in "b" [24 25]]]})))
-    (ct/is (= {"a" [:a :b :e :f :h] "b" [20 21 24 25 27]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["a" "b"] :where [:or [:in "a" [:e :f :h]] [:in "b" [20 21]]]})))
-    (ct/is (= {"a" [] "b" []}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["a" "b"] :where [:in "a" [:z :y]]})))
-    ))
+      (ct/is (= {"b" [24 25 27]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["b"] :where [:in "a" [:e :f :h]]})))
+      (ct/is (= {"b" [24 25 27]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["b"] :where [:and [:in "a" [:e :f :h]]]})))
+      (ct/is (= {"a" [:e :f] "b" [24 25]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["a" "b"] :where [:and [:in "a" [:e :f :h]] [:in "b" [24 25]]]})))
+      (ct/is (= {"a" [:a :b :e :f :h] "b" [20 21 24 25 27]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["a" "b"] :where [:or [:in "a" [:e :f :h]] [:in "b" [20 21]]]})))
+      (ct/is (= {"a" [] "b" []}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["a" "b"] :where [:in "a" [:z :y]]})))
+      )))
 
 (ct/deftest arr-test
-  (ct/testing "array operations")
-  (let [data {"gene" [[:a :b :c] [:a] [:b :c] [:a :c] [:b]]
-              "row" (into [] (range 5))}
-        rows (apply sorted-set (range 5))
-        store {:indexed? #{}
-               :fetch (fn [rows-in field]
+  (ct/testing "array operations"
+    (let [data {"gene" [[:a :b :c] [:a] [:b :c] [:a :c] [:b]]
+                "row" (into [] (range 5))}
+          rows (apply sorted-set (range 5))
+          store {:indexed? #{}
+                 :fetch (fn [rows-in field]
                           (let [r (or rows-in rows)]
                             (zipmap r
                                     (map (data field) r))))}]
-    (ct/is (= {"row" [0 1 3] "gene" [[:a :b :c] [:a] [:a :c]]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["row" "gene"] :where [:in :any "gene" [:a]]})))
-    (ct/is (= {"row" [0 2 3 4]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["row"] :where [:in :any "gene" [:c :b]]})))
-    (ct/is (= {"row" [2 4]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["row"] :where [:in :all "gene" [:c :b]]})))))
+      (ct/is (= {"row" [0 1 3] "gene" [[:a :b :c] [:a] [:a :c]]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["row" "gene"] :where [:in :any "gene" [:a]]})))
+      (ct/is (= {"row" [0 2 3 4]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["row"] :where [:in :any "gene" [:c :b]]})))
+      (ct/is (= {"row" [2 4]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["row"] :where [:in :all "gene" [:c :b]]}))))))
 
 (ct/deftest limit-test
-  (ct/testing "limit clause")
-  (let [data {"gene" [[:a :b :c] [:a] [:b :c] [:a :c] [:b]]
-              "row" (into [] (range 5))}
-        rows (apply sorted-set (range 5))
-        store {:indexed? #{}
-               :fetch (fn [rows-in field]
+  (ct/testing "limit clause"
+    (let [data {"gene" [[:a :b :c] [:a] [:b :c] [:a :c] [:b]]
+                "row" (into [] (range 5))}
+          rows (apply sorted-set (range 5))
+          store {:indexed? #{}
+                 :fetch (fn [rows-in field]
                           (let [r (or rows-in rows)]
                             (zipmap r
                                     (map (data field) r))))}]
-    (ct/is (= {"row" [0 1 2] "gene" [[:a :b :c] [:a] [:b :c]]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["row" "gene"] :where [:in :any "gene" [:a :b]] :limit 3})))
-    (ct/is (= {"row" [1 2 3] "gene" [[:a] [:b :c] [:a :c]]}
-              (sqleval/evaluate rows
-                                store
-                                {:select ["row" "gene"] :where [:in :any "gene" [:a :b]] :limit 3 :offset 1})))))
+      (ct/is (= {"row" [0 1 2] "gene" [[:a :b :c] [:a] [:b :c]]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["row" "gene"] :where [:in :any "gene" [:a :b]] :limit 3})))
+      (ct/is (= {"row" [1 2 3] "gene" [[:a] [:b :c] [:a :c]]}
+                (sqleval/evaluate rows
+                                  store
+                                  {:select ["row" "gene"] :where [:in :any "gene" [:a :b]] :limit 3 :offset 1}))))))
