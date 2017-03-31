75b392da22c631f3b4a9e5645d6240d27ea4dbac
Brian Craft <bcboy@thecraftstudio.com>
2017-03-30 17:02:56 -0700

    Generate benchmark inputs.

--- test/cavm/benchmark/core.clj
+++ test/cavm/benchmark/core.clj
@@ -1,5 +1,6 @@
 (ns cavm.benchmark.core
   (:require [clojure.java.io :as io])
+  (:require [clojure.string :as s])
   (:require [cavm.benchmark.data :as benchmark-data])
   (:require [cavm.benchmark.install :refer [install]])
   (:require [cavm.h2 :as h2])
@@ -16,18 +17,21 @@
   (println msg x)
   x)
 
+(def dataset-map
+  (into {} (map (fn [ds] [(:key ds) ds]) benchmark-data/datasets)))
+
 ; XXX should probably validate the return values to some degree, so
 ; that we can see nothing is broken. Or do that in unit tests?
-(defn probemap-probe [xena dataset probes]
+(defn probemap-probe [xena {id :id} probes]
   (timeit (cdb/column-query xena
                             {:select ["name" "position"]
-                             :from [dataset]
+                             :from [id]
                              :where [:in "name" probes]})))
 
-(defn cnv-gene [xena dataset genes]
+(defn cnv-gene [xena {id :id} genes]
   (timeit (cdb/column-query xena
                             {:select ["position" "sampleID" "value"]
-                             :from [dataset]
+                             :from [id]
                              :where [:in "position" (mapv (juxt :chr :start :end) genes)]})))
 
 (defn sort-genes [genes]
@@ -38,8 +42,88 @@
 (def dbfile
   (str (io/file (System/getProperty "user.home") "xena-benchmark/database")))
 
+(defn datafile [id]
+  (str (io/file (System/getProperty "user.home") "xena-benchmark/files" id)))
+
 (defn report [msg t]
   (format "%60s\t%f msec" msg t))
+
+
+(defn line-count [file]
+  (with-open [rdr (clojure.java.io/reader file)]
+    (count (line-seq rdr))))
+
+(defn rnd-lines [lines size n]
+  (let [select (set (take n (distinct (repeatedly #(rand-int size)))))]
+    (take n (filter identity
+                    (map-indexed (fn [idx itm] (when (select idx) itm))
+                                 lines)))))
+
+; cut fields from tsv, similar to unix 'cut'
+(defn cut [fields lines]
+  (map (fn [line]
+         (let [cols (s/split line #"\t")]
+           (mapv #(cols %) fields)))
+       lines))
+
+; get probes from probemap
+(defn get-probes [{id :id} c]
+  (let [file (datafile id)
+        N (line-count file)]
+    (with-open [rdr (clojure.java.io/reader file)]
+      (doall (flatten (cut [0] (rnd-lines (drop 1 (line-seq rdr)) (dec N) c)))))))
+
+(defn gene-line [[chr start end gene]]
+  {:chr chr
+   :start (Integer/parseInt (s/trim start))
+   :end (Integer/parseInt (s/trim end))
+   :gene gene})
+
+; get genes from refseq, by position.
+; Note this will fail if the column order is different.
+(defn get-genes [{id :id} c]
+  (let [file (datafile id)
+        N (line-count file)]
+    (with-open [rdr (clojure.java.io/reader file)]
+      (doall (map gene-line
+                  (cut [2 4 5 12] (rnd-lines (drop 1 (line-seq rdr)) (dec N) c)))))))
+
+(def tests
+  [^:probemap
+   {:desc "1 probe from probemap of 1.4M rows"
+    :id :probemap-large-one
+    :params (fn []
+            [(get-probes (dataset-map :probemap-large) 1)])
+    :body (fn [xena probes]
+            (probemap-probe xena (dataset-map :probemap-large) probes))}
+   ^:probemap
+   {:desc "2 probe from probemap of 1.4M rows"
+    :id :probemap-large-two
+    :params (fn []
+            [(get-probes (dataset-map :probemap-large) 2)])
+    :body (fn [xena probes]
+            (probemap-probe xena (dataset-map :probemap-large) probes))}
+   ^:cnv
+   {:desc "3 gene positions from CNV of 970k rows"
+    :id :cnv-large-three-gene
+    :params (fn []
+            [(get-genes (dataset-map :refgene-hg19) 3)])
+    :body (fn [xena genes]
+            (cnv-gene xena (dataset-map :cnv-large) genes))}
+   ^:cnv
+   {:desc "30 gene positions from CNV of 970k rows"
+    :id :cnv-large-thirty-gene
+    :params (fn []
+            [(get-genes (dataset-map :refgene-hg19) 30)])
+    :body (fn [xena genes]
+            (cnv-gene xena (dataset-map :cnv-large) genes))}
+   ^:cnv
+   {:desc "300 gene positions from CNV of 970k rows"
+    :id :cnv-large-three-hundred-gene
+    :params (fn []
+            [(get-genes (dataset-map :refgene-hg19) 300)])
+    :body (fn [xena genes]
+            (cnv-gene xena (dataset-map :cnv-large) genes))}])
 
 (defn -main [& args]
   (println "benchmark" args)
@@ -50,15 +134,10 @@
       (let [xena (h2/create-xenadb dbfile)
             {:keys [id probes]} (first benchmark-data/probemaps)
             {cnv-id :id} (first benchmark-data/cnv)]
-        (probemap-probe xena id (take 1 probes)) ; XXX initial query can be slow. Need to investigate.
-        (println (report "1 probe from probemap of 1.4M rows" (probemap-probe xena id (take 1 (drop 5 probes)))))
-        (println (report "2 probe from probemap of 1.4M rows" (probemap-probe xena id (take 2 (drop 10 probes)))))
-        (println (report "3 sorted gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (sort-genes (take 3 benchmark-data/genes)))))
-        (println (report "30 sorted gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (sort-genes (take 30 benchmark-data/genes)))))
-        (println (report "300 sorted gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (sort-genes (take 300 benchmark-data/genes)))))
-        (println (report "3 gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (take 3 benchmark-data/genes))))
-        (println (report "30 gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (take 30 benchmark-data/genes))))
-        (println (report "300 gene positions from CNV of 970k rows" (cnv-gene xena cnv-id (take 300 benchmark-data/genes))))
+        (doseq [{:keys [desc params body]} tests]
+          (let [params (params)
+                res (apply body xena params)]
+            (println (report desc res))))
         ))
     (finally
       (shutdown-agents))))
--- test/cavm/benchmark/data.clj
+++ test/cavm/benchmark/data.clj
@@ -1,50 +1,25 @@
 (ns cavm.benchmark.data
   (:require [clojure.java.io :as io])
-  (:require [clojure.string :as s])
-  )
+  (:require [clojure.string :as s]))
 
 (def probemaps [{:id "AffymetrixHumanExon1ST_hg18"
+                 :key :probemap-large
                  :remote-id "probeMap/AffymetrixHumanExon1ST_hg18"
-                 :host "https://ucscpublic.xenahubs.net"
-                 :probes ["2315223"
-                          "2315102"
-                          "2315232"
-                          "2315200"
-                          "2315116"
-                          "2315165"
-                          "2315238"
-                          "2315162"
-                          "2315126"
-                          "2315120"
-                          "2315244"
-                          "2315200"
-                          "2315195"
-                          "2315183"
-                          "2315188"
-                          "2315156"
-                          "2315236"
-                          "2315132"
-                          "2315179"
-                          "2315150"]}])
+                 :host "https://ucscpublic.xenahubs.net"}])
 
 (def cnv [^:pp {:id "20170119_final_consensus_copynumber_donor"
+                :key :cnv-large
                 :remote-id "20170119_final_consensus_copynumber_donor"
                 :host "\017\021\000\007\033\133\133\126\037\026\017\022\002\112\037\000\032\026\000\024\026\012\101\033\013\021"}])
 
 (def matrix [{:id "AgilentG4502A_07_3"
+              :key :matrix-medium
               :remote-id "TCGA.BRCA.sampleMap/AgilentG4502A_07_3"
               :host "https://tcga.xenahubs.net"}])
 
-(defn tsv-lines [txt]
-  (map #(s/split % #"\t") (s/split txt #"\n")))
+(def refgene [{:id "gencode_good_hg19"
+               :key :refgene-hg19
+               :remote-id "gencode_good_hg19"
+               :host "https://reference.xenahubs.net"}])
 
-(defn gene-line [[chr start end gene]]
-  {:chr chr
-   :start (Integer/parseInt (s/trim start))
-   :end (Integer/parseInt (s/trim end))
-   :gene gene})
-
-(def genes
-  (map gene-line (tsv-lines (slurp (io/resource "benchmark/600genes")))))
-
-(def datasets (concat probemaps cnv matrix))
+(def datasets (concat probemaps cnv matrix refgene))
