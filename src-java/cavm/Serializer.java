6ac8e1cd7d9990e29f95f6343f41af5acb739a44
Brian Craft <craft@soe.ucsc.edu>
2019-09-30 14:52:23 -0700

    Make Serializer polymorphic on compression type

--- src-java/cavm/Serializer.java
+++ src-java/cavm/Serializer.java
@@ -1,14 +1,34 @@
 package cavm;
 
 import cavm.HTFC;
+import cavm.HFC;
+import cavm.GetBytes;
 import org.h2.api.JavaObjectSerializer;
 
 public class Serializer implements JavaObjectSerializer {
+	static final int codeLen = 4;
+	static final byte[] htfc = {104, 116, 102, 99};
+	static final byte[] hfc = {104, 102, 99, 0};
+	private boolean match(byte[] in, byte[] pat) {
+		if (in.length < codeLen) {
+			return false;
+		}
+		for (int i = 0; i < codeLen; ++i) {
+			if (in[i] != pat[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 	public Object deserialize(byte[] bytes) {
-		return new HTFC(bytes);
+		if (match(bytes, htfc)) {
+			return new HTFC(bytes);
+		}
+		// maybe throw here if hfc doesn't match.
+		return new HFC(bytes);
 	}
 	public byte[] serialize(Object obj) {
-		HTFC htfc = (HTFC) obj;
-		return htfc.getBytes();
+		GetBytes gb = (GetBytes) obj;
+		return gb.getBytes();
 	}
 }
